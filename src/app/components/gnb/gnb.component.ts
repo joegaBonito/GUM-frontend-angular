960624ed357ce4de408e741518403f4850ce5447
YOONJAE JUNG/LGEUS Americas Information Center(yoonjae.jung@lge.com) <yoonjae.jung@lge.com>
2017-11-10 15:50:40 -0500

    Dropdown issue has been resolved by replacing GNB code provided by mdbootstrap team

--- src/app/components/gnb/gnb.component.ts
+++ src/app/components/gnb/gnb.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GnbComponent implements OnInit {
 
+  autoClose:boolean = true;
+
   constructor() { }
 
   ngOnInit() {
   }
-
+  onClickDrop($event) {
+    console.log($event.target);
+  }
 }
